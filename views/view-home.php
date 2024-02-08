eed9db63090ad2a95ee050ec2c930a8943095b20
Eloann <eloann76@gmail.com>
2024-02-08 11:33:29 +0100

    organisation du dashboard

--- views/view-home.php
+++ views/view-home.php
@@ -107,6 +107,32 @@
                 <div class="col s12 m6">
                     <div class="card blue-grey darken-1">
                         <div class="card-content white-text">
+                            <span class="card-title">Les 5 derniers trajets :</span>
+                            <?php
+                            // Appeler la fonction pour récupérer les trajets
+                            $fiveTrajets = Entreprise::getFiveLastTrajet($_SESSION['user']['id_entreprise']);
+
+                            // Vérifier si des trajets ont été récupérés
+                            if (!empty($fiveTrajets)) {
+                                // Afficher les trajets
+                                foreach ($fiveTrajets as $trajet) {
+                                    echo "<p><strong>Date :</strong> {$trajet['date_trajet']}</p>";
+                                    echo "<p><strong>Pseudo :</strong> {$trajet['pseudo_participant']}</p>";
+                                    echo "<p><strong>Type :</strong> {$trajet['type_transport']}</p>";
+                                    echo "<p><strong>Distance :</strong> {$trajet['distance_trajet']} km</p>";
+                                    echo "<p><strong>Temps :</strong> {$trajet['temps_trajet']}</p>";
+                                    echo "<hr>";
+                                }
+                            } else {
+                                echo "<p>Aucun trajet trouvé.</p>";
+                            }
+                            ?>
+                        </div>
+                    </div>
+                </div>
+                <div class="col s12 m6">
+                    <div class="card blue-grey darken-1">
+                        <div class="card-content white-text">
                             <span class="card-title">Total des Trajets :</span>
                             <?php
                             // Appeler la fonction pour récupérer les trajets
@@ -149,33 +175,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col s12 m6">
-                    <div class="card blue-grey darken-1">
-                        <div class="card-content white-text">
-                            <span class="card-title">Les 5 derniers trajets :</span>
-                            <?php
-                            // Appeler la fonction pour récupérer les trajets
-                            $fiveTrajets = Entreprise::getFiveLastTrajet($_SESSION['user']['id_entreprise']);
-
-                            // Vérifier si des trajets ont été récupérés
-                            if (!empty($fiveTrajets)) {
-                                // Afficher les trajets
-                                foreach ($fiveTrajets as $trajet) {
-                                    echo "<p><strong>Date :</strong> {$trajet['date_trajet']}</p>";
-                                    echo "<p><strong>Pseudo :</strong> {$trajet['pseudo_participant']}</p>";
-                                    echo "<p><strong>Type :</strong> {$trajet['type_transport']}</p>";
-                                    echo "<p><strong>Distance :</strong> {$trajet['distance_trajet']} km</p>";
-                                    echo "<p><strong>Temps :</strong> {$trajet['temps_trajet']}</p>";
-                                    echo "<hr>";
-                                }
-                            } else {
-                                echo "<p>Aucun trajet trouvé.</p>";
-                            }
-                            ?>
-
-                        </div>
-                    </div>
-                </div>
+                
             </div>
         </div>
     </div>
